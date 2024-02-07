400e67bb858511b379ca8f921e49817e1e39ae5a
PHziinn <skamikaze4@gmail.com>
2024-02-07 00:09:25 -0300

    Alterações

--- config/swagger.js
+++ config/swagger.js
@@ -1,5 +1,6 @@
 import swaggerAutogen from 'swagger-autogen';
 
+
 const doc = {
   info: {
     title: 'My API',
@@ -11,12 +12,7 @@ const doc = {
 const outputFile = './swagger-output.json';
 const routes = ['./src/router/routes.js'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-// swaggerAutogen()(outputFile, routes, doc).then(async () => {
-//   await import('../server.js'); // Your project's root file
-// });
 
 swaggerAutogen()(outputFile, routes, doc);
 
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import {router} from './src/router/routes.js';
+import setupDocumentationRoute from './src/controllers/documentacao.js';
+
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './config/swagger-output.json' assert { type: "json" };
 
 
 
 const app = express();
-
+setupDocumentationRoute(app);
 app.use(express.json());
 app.use(router);
-
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(4000, () => console.log('Servidor Online na Porta 4000'));
